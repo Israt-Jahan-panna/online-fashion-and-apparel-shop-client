21eb76c4b3ebbf17aa27a9a28a879da6b04493e4
Israt Jahan <isratjahanpanna87@gmail.com>
2023-10-25 21:59:11 +0600

    not avaiable product info update

--- src/Pages/BrandPage.jsx
+++ src/Pages/BrandPage.jsx
@@ -1,36 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-import BrandProduct from './BrandProduct';
-import BrandCarousel from './BrandCarousel';
+import BrandProduct from "./BrandProduct";
+import BrandCarousel from "./BrandCarousel";
 
 const BrandPage = () => {
-    const {name} = useParams();
-    const  [brand , setBrand] = useState([]);
- 
- useEffect(()=> {
-    fetch(`https://fashion-and-apparel-shop-server-6ol0g5b2m-israt-jahans-projects.vercel.app/brand/${name}`)
-    .then(res => res.json())
-    .then(data => {
+  const { name } = useParams();
+  const [brand, setBrand] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://fashion-and-apparel-shop-server-6ol0g5b2m-israt-jahans-projects.vercel.app/brand/${name}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
         setBrand(data);
-    })
- },[])
- 
- 
-    return (
-        <div>
-            <div>
-                <BrandCarousel></BrandCarousel>
-            </div>
-           <div className='grid md:grid-cols-2 lg:grid-cols-3'>
-           {
-                brand?.map((product) => { 
-                    return <BrandProduct key={product._id}  product={product} ></BrandProduct>
-                } )
-            }
-           </div>
-        </div>
-    );
+      });
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <BrandCarousel></BrandCarousel>
+      </div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 ">
+        {brand.length > 0 ? (
+          brand.map((product) => (
+            <BrandProduct key={product._id} product={product}></BrandProduct>
+          ))
+        ) : (
+          <div className="flex text-center">
+            <p className="lg:text-3xl font-extrabold  items-center"> we are Sorry <br /> currently this Brands  product not avaiable . <br />Product will be comming soon <br />Thank You for visiting us </p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default BrandPage;
--- src/Pages/BrandProduct.jsx
+++ src/Pages/BrandProduct.jsx
@@ -4,66 +4,74 @@ import { Link } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
 const BrandProduct = ({ product }) => {
+  
+
   const { name, brand, image, price, rating, type, _id } = product || {};
 
   return (
     <div className="mx-24 font-SometypeMono">
-      <div className="text-center">
-        <h4 className="uppercase text-2xl font-extrabold w-full">
-          Product Of {brand}
-        </h4>
-      </div>
-      <div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
-        <div className="relative mx-4 mt-4 overflow-hidden text-white shadow-lg rounded-xl bg-blue-gray-500 bg-clip-border shadow-blue-gray-500/40">
-          <img src={image} alt="ui/ux review check" />
-          <div className="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-tr from-transparent via-transparent to-black/60"></div>
-        </div>
-        <div className="p-3">
-          <div className="flex items-center justify-between mb-3">
+      <h4 className="uppercase text-2xl text-center font-extrabold w-full">
+        Product Of {brand}
+      </h4>
+
+      { brand ? (
+        // Product is available
+        <div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
+          <div className="relative mx-4 mt-4 overflow-hidden text-white shadow-lg rounded-xl bg-blue-gray-500 bg-clip-border shadow-blue-gray-500/40">
+            <img src={image} alt="ui/ux review check" />
+            <div className="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-tr from-transparent via-transparent to-black/60"></div>
+          </div>
+          <div className="p-3">
+            <div className="flex items-center justify-between mb-3">
+              <h5 className="block text-xl antialiased font-medium leading-snug tracking-normal text-blue-gray-900 uppercase">
+                Brand: {brand}
+              </h5>
+              <p className="flex items-center gap-1.5 text-base font-normal leading-relaxed text-blue-gray-900 antialiased">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="-mt-0.5 h-5 w-5 text-yellow-700"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.760-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                {rating}
+              </p>
+            </div>
             <h5 className="block text-xl antialiased font-medium leading-snug tracking-normal text-blue-gray-900 uppercase">
-              Brand: {brand}
+              Name: {name}
             </h5>
-            <p className="flex items-center gap-1.5 text-base font-normal leading-relaxed text-blue-gray-900 antialiased">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                aria-hidden="true"
-                className="-mt-0.5 h-5 w-5 text-yellow-700"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.760-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              {rating}
+            <p className="block text-base antialiased font-medium leading-relaxed text-transparent bg-gradient-to-tr from-pink-600 to-pink-400 bg-clip-text">
+              Price : $ {price}
+            </p>
+            <p className="block text-base antialiased font-light leading-relaxed text-gray-700">
+              Type: {type}
             </p>
           </div>
-          <h5 className="block text-xl antialiased font-medium leading-snug tracking-normal text-blue-gray-900 uppercase">
-            Name: {name}
-          </h5>
-          <p className="block text-base antialiased font-medium leading-relaxed text-transparent bg-gradient-to-tr from-pink-600 to-pink-400 bg-clip-text">
-            Price : $ {price}
-          </p>
-          <p className="block text-base antialiased font-light leading-relaxed text-gray-700">
-            Type: {type}
-          </p>
+          <div className="flex justify-between m-4">
+            <Link to={`/updateproduct/${_id}`}>
+              <button className="btn bg-[#FF324D] rounded-lg mt-3" type="button">
+                Update
+              </button>
+            </Link>
+            <Link to={`/brand/${name}/${_id}`}>
+              <button className="btn bg-[#FF324D] rounded-lg mt-3" type="button">
+                Details
+              </button>
+            </Link>
+          </div>
         </div>
-        <div className="flex justify-between m-4">
-          <Link to={`/updateproduct/${_id}`}>
-          <button className="btn bg-[#FF324D] rounded-lg mt-3" type="button">
-            Update
-          </button>
-          </Link>
-          <Link to={`/brand/${name}/${_id}`}>
-            <button className="btn bg-[#FF324D] rounded-lg mt-3" type="button">
-              Details
-            </button>
-          </Link>
-        </div>
-      </div>
+      ) : (
+        // Product is coming soon
+        <p className="w-10 h-7">This product is coming soon.</p>
+      )}
     </div>
+  
   );
 };
 
